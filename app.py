6955aa05541b5774dcabe4c8b4e6fd9cc41b848f
Aphrx <aphrx@aphrx.ca>
2023-03-11 16:44:20 -0500

    update

--- app.py
+++ app.py
@@ -1,22 +1,42 @@
 import os
+import openai
 from dotenv import load_dotenv
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 from slack_bolt import App
 
 load_dotenv()
 
+app = App(token=os.environ["SLACK_BOT_TOKEN"])
+openai.api_key = os.environ["OPENAI_API_KEY"]
+messages = [
+        {"role": "system", "content": "You are an AI assistant. You are an Australian Shepherd named Hugo who lives on Slack."}
+]
 
-# Initializes your app with your bot token and socket mode handler
-app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
+@app.command("/say-hi")
+def hello_command(ack, body):
+    user_id = body["user_id"]
+    ack(f"Hi, <@{user_id}>!")
 
-# Listens to incoming messages that contain "hello"
-# To learn available listener arguments,
-# visit https://slack.dev/bolt-python/api-docs/slack_bolt/kwargs_injection/args.html
-@app.message("hello")
-def message_hello(message, say):
-    # say() sends a message to the channel where the event was triggered
-    say(f"Hey there <@{message['user']}>!")
+@app.event("app_mention")
+def event_test(say, body):
+    question = str(body['event']['text'])
+    messages.append({'role': 'user', 'content': question})
+    # response = openai.Completion.create(
+    #     model="text-davinci-003",
+    #     prompt=f"The following is a conversation with an AI assistant based on a dog. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: {question}\nAI:",
+    #     temperature=0.9,
+    #     max_tokens=150,
+    #     top_p=1,
+    #     frequency_penalty=0.0,
+    #     presence_penalty=0.6,
+    #     stop=[" Human:", " AI:"]
+    #     ) 
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages
+        )
+    print(response)
+    say(str(response['choices'][0]['message']['content']))
 
-# Start your app
 if __name__ == "__main__":
     SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
